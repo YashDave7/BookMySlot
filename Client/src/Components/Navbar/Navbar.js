6a3f0274ad502febb97d1f35e129f28fd868cf98
Yash Dave <yashdave307@gmail.com>
2023-08-20 20:33:36 +0530

    User login/signup client-server connection done

--- Client/src/Components/Navbar/Navbar.js
+++ Client/src/Components/Navbar/Navbar.js
@@ -1,11 +1,35 @@
+// import './Navbar.css';
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+
+export default function Navbar(props) {
     return (
         <>
-            This is Navbar
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+                <a className="navbar-brand" href="/">BookMySlot</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <form className="form-inline my-2 my-lg-0">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="loginProfessional">Join as a Professional</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/user/login">Login </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/user/signup">Signup </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </>
     )
 }
-
-export default Navbar
---
+++ Client/src/Pages/User-Login-Signup/UserLoginSignin.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import './UserLoginSignin.css';
+
+const UserLoginSignin = () => {
+    let navigate = useNavigate();
+
+    // LOGIN API.
+    const [loginCredentials, setLoginCredentials] = useState({ email: "", password: "" });
+    const onChange = (e) => {
+        setLoginCredentials({ ...loginCredentials, [e.target.name]: e.target.value })
+    }
+
+    const handleLoginSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`http://localhost:5000/api/auth/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email: loginCredentials.email, password: loginCredentials.password })
+            });
+            const json = await response.json()
+            console.log(json);
+            if (json.authToken) {
+                // Save the authToken and Redirect.
+                localStorage.setItem('token', json.authToken);
+                toast.success("Logged In Successfully");
+                navigate("/");
+            }
+            else {
+                toast.error("Invalid credentials");
+            }
+        } catch (error) {
+            console.log("Hello");
+            toast.error("Something went wrong");
+        }
+    }
+
+    // SIGNUP API.
+    const [signupCredentials, setSignupCredentials] = useState({ email: '', name: '', mobile: '', password: '' });
+    const onChangeSignup = (e) => {
+        setSignupCredentials({ ...signupCredentials, [e.target.name]: e.target.value });
+    }
+
+    const handleSignupSubmit = async (e) => {
+        e.preventDefault();
+        if (signupCredentials.password !== signupCredentials.confirmPassword) {
+            console.log(credentials);
+            toast.error("Passwords dont match");
+            return
+        }
+        try {
+            const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ name: signupCredentials.name, email: signupCredentials.email, mobile: signupCredentials.mobile, password: signupCredentials.password })
+            });
+            const json = await response.json()
+            console.log(json);
+            if (json.authToken) {
+                // Save the authToken and Redirect.
+                localStorage.setItem('token', json.authToken);
+                navigate("/");
+                toast.success("Account Created Successfully");
+            }
+            else {
+                toast.error("Invalid credentials");
+            }
+        } catch (error) {
+            toast.error("Something went wrong");
+        }
+    }
+
+    // const [loginStyle, setLoginStyle] = useState('login-toggle');
+    // const [signupStyle, setSignupStyle] = useState('signup-toggle');
+
+    const toggleSignup = () => {
+        console.log("Toggle to Signup");
+    }
+
+    const toggleLogin = () => {
+        console.log("Toggle to Login");
+    }
+
+
+    return (
+        <>
+            <div className="form-modal">
+                <div className="form-toggle">
+                    <button id="" onClick={toggleLogin}>log in</button>
+                    <button id="" onClick={toggleSignup}>sign up</button>
+                </div>
+                <div id="login-form">
+                    <form onSubmit={handleLoginSubmit}>
+                        <input type="email" name='email' onChange={onChange} defaultValue={loginCredentials.email} placeholder="Enter email" />
+                        <input type="password" name='password' onChange={onChange} defaultValue={loginCredentials.password} placeholder="Enter password" />
+                        <button type="submit" className="btn login">login</button>
+                        <p><Link to="">Forgotten account</Link></p>
+                        <hr />
+                    </form>
+                </div>
+                <div id="signup-form">
+                    <form onSubmit={handleSignupSubmit}>
+                        <input type="email" name='email' defaultValue={signupCredentials.email} onChange={onChangeSignup} placeholder="Enter your email" />
+                        <input type="text" name='name' defaultValue={signupCredentials.name} onChange={onChangeSignup} placeholder="Enter your name" />
+                        <input type="number" name='mobile' defaultValue={signupCredentials.mobile} onChange={onChangeSignup} placeholder="Enter your mobile no." />
+                        <input type="password" name='password' defaultValue={signupCredentials.password} onChange={onChangeSignup} placeholder="Create password" />
+                        <input type="password" name='confirmPassword' defaultValue={signupCredentials.confirmPassword} onChange={onChangeSignup} placeholder="Confirm password" />
+                        <button type="submit" className="btn signup">create account</button>
+                        <p>Clicking <strong>create account</strong> means that you are agree to our <Link to="">terms of services</Link>.</p>
+                        <hr />
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default UserLoginSignin
