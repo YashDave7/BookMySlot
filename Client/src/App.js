b0ee3c48a46331eafc067c1fa73256c206c34458
Yash Dave <yashdave307@gmail.com>
2023-08-20 20:32:18 +0530

    ...

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,23 @@
+import Navbar from './Components/Navbar/Navbar';
+import UserLoginSignin from './Pages/User-Login-Signup/UserLoginSignin';
+import LandingPage from './Pages/Landing-Page/LandingPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ToastNotifications from './Components/ToastNotifications';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <ToastNotifications />
+        <Routes>
+          <Route exact path='/' element={ <LandingPage /> } />
+          <Route exact path='/user/login' element={ <UserLoginSignin /> } />
+          <Route exact path='/user/signup' element={ <UserLoginSignin /> } />
+        </Routes>
+      </Router>
+
+    </>
   );
 }
 
