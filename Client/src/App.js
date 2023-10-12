2c60972cfe3c17c62eb78fefd6402e6c492ac641
asmigawade <109681992+asmigawade@users.noreply.github.com>
2023-10-12 20:04:14 +0530

    new changes

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,7 +6,11 @@ import ToastNotifications from './Components/ToastNotifications';
 import ProfessionalLogin from './Pages/Professional-Login-Registration/ProfessionalLogin';
 import UserHomePage from './Pages/User-Home-Page/UserHomePage';
 import ProfessionalHomePage from './Pages/Professional-Home-Page/ProfessionalHomePage';
-
+import DoctorDashboard from './Pages/Doctor-Professional-Side/DoctorDashboard';
+import LawyerDashboard from './Pages/Lawyer-Professional-side/LawyerDashboard';
+import CounsellorDashboard from './Pages/Counsellor-Professional-Side/CounsellorDashboard';
+import '../src/App.css'
+import React, { useContext } from 'react';
 function App() {
   return (
     <>
