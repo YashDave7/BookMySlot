43ca647fe507036345fe6618c4e043b061ae2079
BasavrajD <dhamanagebasavraj@gmail.com>
2023-10-15 22:13:41 +0530

    particular Professional

--- Client/src/Pages/Professional-Page-templete/Professional.js
+++ Client/src/Pages/Professional-Page-templete/Professional.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import MyCalendar from "../Professional-Home-Page/MyCalendar";
 // import { Link, useNavigate } from 'react-router-dom';
 // import { isHtmlElement } from 'react-router-dom/dist/dom';
 
@@ -10,7 +11,6 @@ const Professional = (props) => {
 
   const particularProfessional = async () => {
     try {
-
       // API call.
       const response = await fetch(
         `http://localhost:5000/api/authProfessional/particularProfessional/${profID}`,
@@ -38,35 +38,98 @@ const Professional = (props) => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "auth-token": localStorage.getItem('token')
+          "auth-token": localStorage.getItem("token"),
         },
       }
     );
     const json = await response.json();
     console.log(json);
     setProfessional(json);
-  }
+  };
 
   useEffect(() => {
-    if (localStorage.getItem('token')) {
+    if (localStorage.getItem("token")) {
       particularProfessional();
       //eslint-disable-next-line
+    } else {
+      navigate("/user/login");
     }
-    else {
-      navigate('/user/login');
-    }
-  }, [props.profId])
+  }, [props.profId]);
 
+  //time slot
+  const [selectedTimeSlots, setSelectedTimeSlots] = useState([]);
+
+  // Define your time slots as an array
+  const timeSlots = [
+    "10:00 AM - 11:00 AM",
+    "11:00 AM - 12:00 PM",
+    "12:00 PM - 01:00 PM",
+    // Add more time slots here...
+  ];
+
+  const handleTimeSlotClick = (timeSlot) => {
+    // Check if the time slot is already selected
+    if (selectedTimeSlots.includes(timeSlot)) {
+      setSelectedTimeSlots(
+        selectedTimeSlots.filter((slot) => slot !== timeSlot)
+      );
+    } else {
+      setSelectedTimeSlots([...selectedTimeSlots, timeSlot]);
+    }
+  };
   return (
     <>
-      <div>
+      {/* <div>
         Heoooooollllo
         {professional.name},
         {professional.profession}
       </div>
-      <button onClick={bookAppointment}>Book Appointment</button>
-    </>
-  )
-}
+      <button onClick={bookAppointment}>Book Appointment</button> */}
 
-export default Professional
+      <div className="container-fluid no-padding text-center">
+        <div className="row">
+          <div className="col-8" style={{ border: "2px solid red" }}>
+            profile
+          </div>
+          <div className="col-4" style={{ border: "2px solid red" }}>
+            <MyCalendar />
+          </div>
+
+          {/* <!-- Force next columns to break to new line at md breakpoint and up --> */}
+          <div className="w-100 d-none d-md-block"></div>
+
+          <div className="col-8" style={{ border: "2px solid red" }}>
+            .col-6 .col-sm-4
+          </div>
+          <div className="col-4" style={{ border: "2px solid red" }}>
+            <h3>Available Slots</h3>
+            <div className="container">
+              {timeSlots.map((timeSlot, index) => (
+                <div key={index} className="col-md-4">
+                  <button
+                    style={{
+                      backgroundColor: selectedTimeSlots.includes(timeSlot)
+                        ? "#9AA4EC"
+                        : "transparent",
+                      color: selectedTimeSlots.includes(timeSlot)
+                        ? "white"
+                        : "black",
+                    }}
+                    className="btn time-slot my-2"
+                    onClick={() => handleTimeSlotClick(timeSlot)}
+                  >
+                    {timeSlot}
+                  </button>
+                </div>
+              ))}
+                <button className="btn btn-success">Book My Slot</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Professional;
