6dbefdee8627a26ac6bec9f0242e5b1b17e89c84
Yash Dave <yashdave307@gmail.com>
2023-10-15 22:18:04 +0530

    professional home

--- Client/src/Pages/Professional-Home-Page/ProfessionalHomePage.js
+++ Client/src/Pages/Professional-Home-Page/ProfessionalHomePage.js
@@ -5,14 +5,17 @@ import { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import './style/MyCalendar.css';
 import ProfDashboard from './ProfDashboard';
-import TodaysAppointmets from './TodaysAppointmets';
+// import TodaysAppointmets from './TodaysAppointmets';
+import user_img from "../../Pages/User-Home-Page/images/reshma.png";
 
 const ProfessionalHomePage = () => {
 
   const [appointments, setAppointments] = useState([]);
+  const [professionalDetails, setProfessionalDetails] = useState('');
   const [filteredAppointments, setFilteredAppointments] = useState([]);
   const [date, setDate] = useState(new Date());
 
+  // API CALL TO FETCH ALL THE APPOINTMENTS.
   const fetchAppointments = async () => {
     // API call.'
     const response = await fetch(`http://localhost:5000/api/professionalRoutes/fetchAppointments`, {
@@ -27,6 +30,7 @@ const ProfessionalHomePage = () => {
     setAppointments(json);
   }
 
+  // API CALL TO CANCEL A APPOINTMENT BY PROFESSIONAL.
   const cancelAppointment = async (appointmentId) => {
     try {
       // API call.
@@ -50,9 +54,29 @@ const ProfessionalHomePage = () => {
     }
   };
 
+  // API CALL TO GET LOGGED IN PROFESSIONAL'S DETAILS.
+  const getProfessionalDetails = async () => {
+    try {
+      // API call.
+      const response = await fetch(`http://localhost:5000/api/authProfessional/getprofessional`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": localStorage.getItem('token')
+        },
+      });
+      const json = await response.json();
+      setProfessionalDetails(json);
+      console.log(professionalDetails);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     if (localStorage.getItem('token')) {
       fetchAppointments()
+      getProfessionalDetails();
       // eslint-disable-next-line
     }
     else {
@@ -75,16 +99,92 @@ const ProfessionalHomePage = () => {
       <div className='container-fluid no-padding'>
         <div className='row'>
           <div className='side_bar col-2'>
-            <ProfDashboard/>
+            <ProfDashboard />
           </div>
           <div className='col-10'>
-            <div class="container-fluid no-padding">
-              <div class="row">
-                <div class="col-12" style={{ border: "2px solid red", height: '15vh'}}>Welcome Message</div>
-                <div class="col-8" style={{ border: "2px solid red" }}>
-                    <TodaysAppointmets/>
+            <div className="container-fluid no-padding">
+              <div className="row">
+                <div className="col-12" style={{ border: "2px solid red", height: '15vh' }}>
+                  <div className="message">
+                    {/* <img src={doctor} /> */}
+                    <h1>Hello, {professionalDetails.name}</h1> 
+                    <p>You have {filteredAppointments.length} appointments today.</p>
+                  </div>
                 </div>
-                <div class="col-4" style={{ border: "2px solid red" }}>
+                <div className="col-8" style={{ border: "2px solid red" }}>
+                  {/* <TodaysAppointmets/> */}
+                  <div>
+                    <h2 className="py-3"> {date.toDateString()} Appointments</h2>
+                    <div>
+                      {filteredAppointments.map((item, i) => (
+                        <div
+                          className="card mb-3 mt-3"
+                          style={{ width: "100%", boxShadow: "0 0 10px grey" }}
+                        >
+                          <div className="row g-0">
+                            <div className="col-md-2">
+                              <img src={user_img} className="img-fluid rounded-start" alt="..." />
+                            </div>
+                            <div className="col-md-8">
+                              <div className="card-body">
+                                <h5 className="card-title" style={{ fontWeight: 700 }}>
+                                  {item.username}
+                                </h5>
+                                <p className="card-text">
+                                  <span
+                                    className="px-2 py-2 text-white mr-3"
+                                    style={{ backgroundColor: "#F4A4A4", borderRadius: "7px" }}
+                                  >
+                                    10AM to 11AM
+                                  </span>
+
+
+                                </p>
+                                {/* <p className="card-text">Student</p> */}
+                                <p className="card-text text-secondary">
+                                  <button
+                                    className="btn text-white py-2"
+                                    style={{
+                                      backgroundColor: "#D83A22",
+                                      fontWeight: 700,
+                                      border: "1px solid black",
+                                      marginRight: "10px"
+                                    }}
+                                  >
+                                    Completed
+                                  </button>
+                                  <button
+                                    className="btn text-white py-2"
+                                    style={{
+                                      backgroundColor: "#D83A22",
+                                      fontWeight: 700,
+                                      border: "1px solid black",
+                                    }}
+                                  >
+                                    Cancel
+                                  </button>
+                                </p>
+                              </div>
+                            </div>
+                            <div className="col-md-2 m-auto">
+                              <button
+                                className="btn text-white py-2"
+                                style={{
+                                  backgroundColor: "#D83A22",
+                                  fontWeight: 700,
+                                  border: "1px solid black",
+                                }}
+                              >
+                                Review
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <div className="col-4" style={{ border: "2px solid red" }}>
                   <div className="Calendar">
                     <h1>Calendar</h1>
                     <Calendar
@@ -98,10 +198,6 @@ const ProfessionalHomePage = () => {
           </div>
         </div>
       </div>
-      
-      {/* <MyCalendar /> */}
-
-
 
       {/* <div className="all_camps">
         {filteredAppointments.map((item, i) => (
@@ -111,7 +207,7 @@ const ProfessionalHomePage = () => {
               <h5 className="card-title">{item.username}</h5>
               {/* </div> 
               <div className='d-flex justify-content-between mb-0'>
-                <span class="badge text-bg-secondary" style={{ "color": "#F4EEE0" }}>{item.email}</span>
+                <span className="badge text-bg-secondary" style={{ "color": "#F4EEE0" }}>{item.email}</span>
                 <div>
                   <i className="fa-solid fa-pen-to-square" style={{ "fontSize": "20px" }}>
                     {item.useremail}</i>
