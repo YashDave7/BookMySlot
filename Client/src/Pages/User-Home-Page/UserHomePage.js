97f6c01568ad7ce115e3a1443c20a55d94c780be
Yash Dave <yashdave307@gmail.com>
2023-10-15 15:11:35 +0530

    ...

--- Client/src/Pages/User-Home-Page/UserHomePage.js
+++ Client/src/Pages/User-Home-Page/UserHomePage.js
@@ -40,38 +40,46 @@ const UserHomePage = ({ setProfId }) => {
 
   return (
     <>
-      <div>
-        {professionals.map((item, i) => (
-          <div className="card">
-            <div className="card-image">
+      <div className="ad">
+        Advertisement
+      </div>
+      <div className='main'>
+        <div className='side-ad'>
+          Advertisement
+        </div>
+        <div className='main-card'>
+          {professionals.map((item, i) => (
+            <div onClick={() => goToProfessional(item._id)} className="card">
+              <div className="card-image">
 
+              </div>
+              <div className="card-info">
+                <div className="prof-name">
+                  {item.name}
+                </div>
+                <div className="rating">
+                  <span className="rate-avg">4.8</span>
+                  <span>124 Ratings</span>
+                </div>
+                <div className="prof-city">
+                  {item.city}
+                </div>
+                <div className="prof-specialisation">
+                  <span>{item.specialisation}</span>
+                  {/* <span>Glaucoma</span> */}
+                </div>
+                <div className="book">
+                  <button>Book Appointment</button>
+                </div>
+              </div>
             </div>
-            <div className="card-info">
-              <div className="prof-name">
-                {item.name}
-              </div>
-              <div className="rating">
-                <span className="rate-avg">4.8</span>
-                <span>124 Ratings</span>
-              </div>
-              <div className="prof-city">
-                {item.city}
-              </div>
-              <div className="prof-specialisation">
-                <span>{item.specialisation}</span>
-                {/* <span>Glaucoma</span> */}
-              </div>
-              <div className="book">
-                <button>Book Appointment</button>
-              </div>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
 
         <div className="all_camps">
           {/* {professionals.map((item, i) => (
             <div onClick={() => goToProfessional(item._id)}>
-                <h5 className="card-title">{item.name}</h5>
+            <h5 className="card-title">{item.name}</h5>
               
                 <div className='d-flex justify-content-between mb-0'>
                   <span class="badge text-bg-secondary" style={{ "color": "#F4EEE0" }}>{item.email}</span>
