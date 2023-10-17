b10db5869b8b84ce84b637bbe164a92da0d23966
BasavrajD <dhamanagebasavraj@gmail.com>
2023-10-17 22:43:43 +0530

    your appointments Page

--- Client/src/Pages/User-Appointments-Page/UserAppointments.js
+++ Client/src/Pages/User-Appointments-Page/UserAppointments.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import user_image from '../../Pages/User-Home-Page/images/reshma.png'
 
 const UserAppointments = ({ setProfId }) => {
     let navigate = useNavigate();
@@ -55,15 +56,15 @@ const UserAppointments = ({ setProfId }) => {
 
     return (
         <>
-            <div>
+            {/* <div>
                 <h2>Your Appointments</h2>
                 <div className="all_camps">
                     {appointments.map((item, i) => (
                         <div>
                             <div className="card-body">
-                                {/* <div className='d-flex justify-content-between align-items-center'> */}
+                                {/* <div className='d-flex justify-content-between align-items-center'> 
                                 <h5 className="card-title">{item.professionalname}</h5>
-                                {/* </div> */}
+                                {/* </div> 
                                 <div className='d-flex justify-content-between mb-0'>
                                     <span class="badge text-bg-secondary" style={{ "color": "#F4EEE0" }}>{item.email}</span>
                                     <div>
@@ -82,6 +83,75 @@ const UserAppointments = ({ setProfId }) => {
 
                         </div>
                     ))}
+                </div>
+            </div> */}
+
+            <h2>Your Appointments</h2>
+            <hr/>
+            <div className='card-group'>
+                <div class="card mx-3" style={{boxShadow: '0 0 10px grey'}}>
+                    <img src="..." class="card-img-top img-fluid" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Dr. Reshma Malik</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+                <div class="card mx-3" style={{boxShadow: '0 0 10px grey'}}>
+                    <img src="..." class="card-img-top img-fluid" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title" style={{fontWeight: 700}}>Dr. Reshma Malik</h5>
+                        <p class="card-text">Profession, Specialization</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><span>Date</span>:</li>
+                        <li class="list-group-item">Time Slot:</li>
+                        <li class="list-group-item">Contact</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Cancel Appointment</a>
+                        
+                    </div>
+                </div>
+                <div class="card mx-3" style={{boxShadow: '0 0 10px grey'}}>
+                    <img src="..." class="card-img-top img-fluid" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Dr. Reshma Malik</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+                <div class="card mx-3" style={{boxShadow: '0 0 10px grey'}}>
+                    <img src="..." class="card-img-top img-fluid" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Dr. Reshma Malik</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
                 </div>
             </div>
         </>
