e88c7c823fadc0174a8a2b7789a5ba0a86ec0f00
BasavrajD <dhamanagebasavraj@gmail.com>
2023-10-18 10:36:21 +0530

    particular_prof

--- Client/src/Pages/Professional-Page-templete/Professional.js
+++ Client/src/Pages/Professional-Page-templete/Professional.js
@@ -195,9 +195,9 @@ const Professional = (props) => {
 
       <div className="container-fluid no-padding">
         <div className="row">
-          <div className="col-8" style={{ border: "2px solid red" }}>
-            <h3>Profession Profile</h3>
-            <div className="card mb-3 mt-3" style={{ width: '100%', boxShadow: '0 0 10px grey'}}>
+          <div className="col-8" >
+            <h3 className="mt-3">Profession Profile</h3>
+            <div className="card mb-3 mt-3" style={{ width: '100%', boxShadow: '0 0 10px grey',borderRadius: '10px'}}>
               <div className="row g-0">
                 <div className="col-md-2">
                   <img src={user_img} className="img-fluid rounded-start" alt="..." />
@@ -243,8 +243,10 @@ const Professional = (props) => {
                 </div>
               </div>
             </div>
+            <hr className="mt-5" style={{border: '1px dashed #9AA4EC'}}/>
+
           </div>
-          <div className="col-4" style={{ border: "2px solid red" }}>
+          <div className="col-4 mt-2" >
             <Calendar
               onChange={onChange}
               value={date}
@@ -254,14 +256,51 @@ const Professional = (props) => {
           {/* <!-- Force next columns to break to new line at md breakpoint and up --> */}
           <div className="w-100 d-none d-md-block"></div>
 
-          <div className="col-8" style={{ border: "2px solid red" }}>
-            <div className="card mb-3 mt-3" style={{ width: '100%', boxShadow: '0 0 10px grey'}}>
-              <h1>About Us</h1>
+          <div className="col-8" >
+            <div className="card mb-3 mt-3" style={{ width: '100%', border: 'none'}}>
+              <h3>Reviews</h3>
+              <div className="card mb-3 mt-3" style={{ width: '100%', boxShadow: '0 0 3px grey', borderRadius: '20px'}}>
+              <div className="row g-0">
+                <div className="col-md-2">
+                  <img src={user_img} className="img-fluid rounded-start" alt="..." style={{height:'80px'}}/>
+                </div>
+                <div className="col-md-7">
+                  <div className="card-body">
+                    <h5 className="card-title" style={{fontWeight: 700}}>Yash Dave</h5>
+
+                    <p className="card-text">
+                      Heart is feeling good now!!
+                    </p>
+
+                  </div>
+                </div>
+                <div className="col-md-3 m-auto">
+                      <span className='px-2 py-1 text-white mr-3' style={{backgroundColor: '#F4A4A4', borderRadius: '7px'}}>4.5</span>
+                      <svg  xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#4EA1D3" class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                      </svg>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#4EA1D3" class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                      </svg>
+                      <svg  xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#4EA1D3" class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                      </svg>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#4EA1D3" class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                      </svg>
+                      <svg  xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#4EA1D3" class="bi bi-star-half" viewBox="0 0 16 16">
+                        <path d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z"/>
+                      </svg>
+                </div>
+              </div>
+            </div>
             </div>
           </div>
 
-          <div className="col-4" style={{ border: "2px solid red" }}>
-            <h3>Available Slots</h3>
+
+          <div className="col-4 " >
+            
+            <h3 className="mt-5">Available Slots</h3>
             <div className="container">
               {filteredTimeSlots.map((slot, index) => (
                 <div key={index} className="col-md-4">
